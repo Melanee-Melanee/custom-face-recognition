52635c01b16b82ce14f53631c4e6ce6a89f74e0f
Melanee <74653444+Melanee-Melanee@users.noreply.github.com>
2024-09-23 16:09:24 +0330

    Update custom face recognition.py

--- custom face recognition.py
+++ custom face recognition.py
@@ -1,8 +1,10 @@
-import cv2
-import numpy
-import face_recognition
 # pip install face_recognition==1.3.0
 # pip install numpy==1.26.4
 # pip install opencv-python==4.10.0.84
+
+import cv2
+import numpy
+import face_recognition
+
 
 
