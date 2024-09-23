7bd077a6a66972de538d6582e3fb58a25ac95541
Melanee <74653444+Melanee-Melanee@users.noreply.github.com>
2024-09-23 15:15:30 +0330

    Update and rename custom face detection.py to custom face recognition.py

--- custom face recognition.py
+++ custom face recognition.py
@@ -1,7 +1,7 @@
 import cv2
 import numpy
-import face_detection
-# pip install face_detection==1.3.0
+import face_recognition
+# pip install face_recognition==1.3.0
 # pip install numpy==1.26.4
 # pip install opencv-python==4.10.0.84
 
