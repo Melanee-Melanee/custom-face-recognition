65a8dc3f0bc422319d673cf335ff733507413384
Melanee <74653444+Melanee-Melanee@users.noreply.github.com>
2024-09-23 10:24:08 +0330

    Update custom face detection.py

--- custom face detection.py
+++ custom face detection.py
@@ -1,2 +1,9 @@
+#Date of project: Nov 2021
 import cv2
+import numpy
 import face_detection
+# pip install face_detection==1.3.0
+# pip install numpy==1.21.4
+# pip install opencv-python==4.5.4.60
+
+
